b200e8930a5b3594920094af7a6a293fd209c1de
Osama Selim <osamaselim216@gmail.com>
2025-11-08 22:01:51 +0200

    added app colors file

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  const AppColors._();
+  static const Color black = Colors.black;
+  static const Color transparent = Colors.transparent;
+  static const Color grey = Colors.grey;
+  static Color darkGrey = Colors.grey[700]!;
+  static const Color white = Colors.white;
+  static const Color kPrimaryLightColor = Color.fromARGB(255, 165, 221, 255);
+  static const Color kPrimaryColor = Color.fromARGB(255, 78, 187, 255);
+}
