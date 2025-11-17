2f0cba5bd30fccabe8c5ada72b6ac3efa5726333
Osama Selim <osamaselim216@gmail.com>
2025-11-17 20:49:18 +0200

    -changed feature name to CategoryNews

--- lib/features/category_news/presentation/widgets/categories_body.dart
+++ lib/features/category_news/presentation/widgets/categories_body.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsly/core/constants/constants.dart';
-import 'package:newsly/features/categories/presentation/widgets/categories_grid_view.dart';
+import 'package:newsly/features/category_news/presentation/widgets/categories_grid_view.dart';
 
 class CategoriesBody extends StatelessWidget {
   const CategoriesBody({super.key});
--- lib/features/category_news/presentation/widgets/categories_grid_view.dart
+++ lib/features/category_news/presentation/widgets/categories_grid_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:newsly/core/models/category_model.dart';
-import 'package:newsly/features/categories/presentation/widgets/category_item_widget.dart';
+import 'package:newsly/features/category_news/presentation/widgets/category_item_widget.dart';
 
 class CategoriesGridView extends StatelessWidget {
   const CategoriesGridView({super.key, required this.categories});
